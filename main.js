d2ea24c148a5019fb590754f83c9c028b6771259
vaiperlexmar <vaiperlexmar@gmail.com>
2023-12-07 20:35:52 +0300

    Made modal window for new books adding

--- main.js
+++ main.js
@@ -38,7 +38,29 @@ function Book(author, title, pages, isRead) {
   this.isRead = isRead;
 }
 
-function addBookToTheLibrary() {}
+const newBookModal = document.querySelector(".modal");
+const closeNewBookModal = document.querySelector(".modal__close");
+closeNewBookModal.addEventListener("click", () => {
+  newBookModal.classList.add("hidden");
+});
+const modalForm = document.forms[0];
+const submitNewBook = document.querySelector(".modal__submit");
+
+function addBookToTheLibrary() {
+  document.body.classList.add("modal-open");
+  newBookModal.classList.remove("hidden");
+
+  submitNewBook.addEventListener("click", (e) => {
+    e.preventDefault();
+    const author = modalForm[0].value;
+    const title = modalForm[1].value;
+    const pages = modalForm[2].value;
+    const isRead = modalForm[3].checked;
+    myLibrary.push(new Book(author, title, pages, isRead));
+    document.body.classList.remove("modal-open");
+    newBookModal.classList.add("hidden");
+  });
+}
 
 // Add content to page
 const libraryEl = document.querySelector(".library");
